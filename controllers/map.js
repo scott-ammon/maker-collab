f4e5a1565f1e4f9cb70a900a579ced4030b12670
Scott Ammon <scott.o.ammon@gmail.com>
2018-06-30 08:27:43 -0700

    removed map center and zoom variables as they are not needed now that page doesnt refresh

--- controllers/map.js
+++ controllers/map.js
@@ -6,48 +6,32 @@ var async = require('async');
 
 // GET - returns map showing all projects
 router.get('/', isLoggedIn, function(req, res) {
-  console.log('\x1b[36m%s\x1b[0m', 'main map get route hit');
-
   db.project.findAll({
     include: [db.tag]
   }).then(function(projects) {
-    var mapboxToken = process.env.MAPBOX;
-    // default center and zoom over the United States for now
-    var mapCenter = [-98, 38];
-    var mapZoom = 4;
-      res.render('map/index', {projects: projects, mapboxToken: mapboxToken, mapCenter: mapCenter, mapZoom: mapZoom});
+      var mapboxToken = process.env.MAPBOX;
+      res.render('map/index', {projects: projects, mapboxToken: mapboxToken});
     });
 });
 
 // POST - read filter form data and return projects with associated tags to map
 router.post('/filter', isLoggedIn, function(req, res) {
-  // save map center and zoom level to pass back to front
-  console.log("\x1b[33m",'MAP CENTER', req.body.mapC);
-  console.log("\x1b[33m",'MAP ZOOM', req.body.mapZ);
-  console.log("\x1b[33m",'TAG FILTER', req.body.tagFilter);
-  console.log("\x1b[33m", 'filter route hit');
-
   db.tag.find({
     where: {tagName: req.body.tagFilter}
   }).then(function(tag) {
     if(tag) {
       tag.getProjects().then(function(projects) {
         var mapboxToken = process.env.MAPBOX;
-        console.log("JUST ABOUT TO RENDER THE MAP...");
-        res.json({
-          projects: projects,
-          mapC: req.body.mapC,
-          mapZ: req.body.mapZ
-        });
-        // res.render('map/index', {projects: projects, mapboxToken: mapboxToken, mapCenter: req.body.mapC, mapZoom: req.body.mapZ});
+        // send filtered projects to front instead of rendering a new page
+        res.json({projects: projects});
       }).catch(function(err) {
         console.log("ERROR: ", err)
       });
     } else {
       var mapboxToken = process.env.MAPBOX;
+      // send empty projects array to front so we don't error out on projects undefined
       projects = [];
-      console.log("JUST ABOUT TO RENDER THE MAP (no tag)...");
-      res.render('map/index', {projects: projects, mapboxToken: mapboxToken, mapCenter: req.body.mapC, mapZoom: req.body.mapZ});
+      res.json({projects: projects});
     }
   });
 });
--- public/js/createMap.js
+++ public/js/createMap.js
@@ -1,10 +1,11 @@
 mapboxgl.accessToken = mapboxToken;
 
+// generate new Mapbox map and geocoder search bar
 var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/scottammon/cjit4mrto3t7j2rmsf423fo2u',
-        center: mapCenter,
-        zoom: mapZoom
+        center: [-98, 38],
+        zoom: 4
       });
 
       // Adds geocoding control bar on map to zoom to locations
@@ -14,50 +15,50 @@ var map = new mapboxgl.Map({
 
 var markerArray = [];
 
-// hijack post route and add current map state to form data
+// add all projects to map on main get route to '/map'
+projects.forEach(function(project) {
+  if(project.lng && project.lat) {
+    var marker = new mapboxgl.Marker()
+    .setLngLat([project.lng, project.lat])
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML("<a href='/projects/" + project.id + "'>" + project.title + "</a>"))
+    .addTo(map);
+   markerArray.push(marker);
+  }
+});
+
+// hijack post route to reload map with new data based on selected tag
 $(".filter-map").on('submit', function(e) {
+  // prevent page refresh
   e.preventDefault();
 
-  console.log('map center:', map.getCenter());
-  console.log('map zoom:', map.getZoom());
-
-  var mapCenter = [map.getCenter().lng, map.getCenter().lat];
-  var mapZoom = map.getZoom();
-
-  // set the map center and zoom level in the form inputs before passing to server
-  $(".map-center").attr('value', mapCenter);
-  $(".map-zoom").attr('value', mapZoom);
-
   var newData = $(this).serialize();
-
   var url = $(this).attr('action');
-    $.ajax({
-      method: 'POST',
-      url: url,
-      data: newData
-    }).done(function(data) {
-      // window.location = '/map';
-      var projects = data.projects;
-      var mapCenter = data.mapC;
-      var mapZoom = data.mapZ;
-  
-      mapCenter = mapCenter.split(',');
-      mapCenter = mapCenter.map(num => parseInt(num));
 
-      markerArray.forEach(function(marker) {
-        marker.remove();
-      });
+  // async call to the server side post route, passing the current map state
+  $.ajax({
+    method: 'POST',
+    url: url,
+    data: newData
+  }).done(function(data) {
+    // server post route sends JSON data back with projects
 
-      projects.forEach(function(project) {
-        if(project.lng && project.lat) {
-         var marker = new mapboxgl.Marker()
-          .setLngLat([project.lng, project.lat])
-         .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-         .setHTML("<a href='/projects/" + project.id + "'>" + project.title + "</a>"))
-         .addTo(map);
-         markerArray.push(marker);
-        }
-      });
+    // remove markers from map
+    markerArray.forEach(function(marker) {
+      marker.remove();
     });
+
+    // add markers for the filtered projects that were passed back
+    data.projects.forEach(function(project) {
+      if(project.lng && project.lat) {
+       var marker = new mapboxgl.Marker()
+        .setLngLat([project.lng, project.lat])
+       .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+       .setHTML("<a href='/projects/" + project.id + "'>" + project.title + "</a>"))
+       .addTo(map);
+       markerArray.push(marker);
+      }
+    });
+  });
 });
 
